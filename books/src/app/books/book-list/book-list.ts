79900de8263cb3932bf1e4ee558c0662fa92e25a
Peter Dickten <peter.dickten@dcs-fuerth.de>
2025-11-12 09:12:37 +0100

    WIP

--- books/src/app/books/book-list/book-list.ts
+++ books/src/app/books/book-list/book-list.ts
@@ -14,17 +14,20 @@ export class BookList {
       isbn: '123',
       title: 'Angular 18',
       price: 19,
-      rating: 3.5
+      rating: 3.5,
+      coverUrl: 'https://m.media-amazon.com/images/I/71Wv+d6oP6L._AC_UY436_QL65_.jpg'
     },     {
       isbn: '124',
       title: 'Angular 19',
       price: 21,
-      rating: 4.0
+      rating: 4.0,
+      coverUrl: 'https://m.media-amazon.com/images/I/71wlgd2ShsL._SY522_.jpg'
     }, {
       isbn: '125',
       title: 'Angular 20',
       price: 25,
-      rating: 4.5
+      rating: 4.5,
+      coverUrl: 'https://m.media-amazon.com/images/I/71le4bCnY1L._SY522_.jpg'
     }
   ];
 }
