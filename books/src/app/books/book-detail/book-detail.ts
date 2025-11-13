05b977b15cf4c9b6a48062fe0b31d46cf51afea2
Peter Dickten <peter.dickten@dcs-fuerth.de>
2025-11-13 11:04:49 +0100

    WIP

--- books/src/app/books/book-detail/book-detail.ts
+++ books/src/app/books/book-detail/book-detail.ts
@@ -1,5 +1,7 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
+import { BookData } from '../book-data';
+import { Book } from '../book';
 
 @Component({
   selector: 'book-book-detail',
@@ -7,11 +9,11 @@ import { ActivatedRoute } from '@angular/router';
   templateUrl: './book-detail.html',
   styleUrl: './book-detail.css',
 })
-export class BookDetail {
-
+export class BookDetail implements OnInit {
   isbn: string = '';
+  book: Book | null = null;
 
-  constructor(private route: ActivatedRoute) {
+  constructor(private route: ActivatedRoute, private bookDataService: BookData) {
     // async
     this.route.params.subscribe((params) => {
       console.log(params);
@@ -19,6 +21,25 @@ export class BookDetail {
     });
 
     this.isbn = this.route.snapshot.paramMap.get('isbn') || '';
- }
+  }
+
+  async ngOnInit(): Promise<void> {
+    try {
+      this.book = await this.bookDataService.getBook(this.isbn);
+      console.log('ngOnInit', this.book);
+    } catch (error) {
+      console.error('Fehler beim Laden des Buches', error);
+    }
+  }
+
+  ngOnInitOhneAsync() {
+    const promiseBook = this.bookDataService.getBook(this.isbn);
+    promiseBook.then((book) => {
+      this.book = book;
+      console.log('ngOnInit2', this.book);
+    }).catch((error) => {
+      console.error('Fehler beim Laden des Buches', error);
+    });
+  }
 
 }
