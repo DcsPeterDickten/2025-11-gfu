e8949affda05d0ca3926d5395b640e6652a91eb3
Peter Dickten <peter.dickten@dcs-fuerth.de>
2025-11-13 11:42:33 +0100

    WIP

--- books/src/app/app.routes.ts
+++ books/src/app/app.routes.ts
@@ -3,11 +3,12 @@ import { Start } from './books/start/start';
 import { BookList } from './books/book-list/book-list';
 import { Calculator } from './shared/calculator/calculator';
 import { BookDetail } from './books/book-detail/book-detail';
+import { bookGuard } from './books/book-guard';
 
 export const routes: Routes = [
 { path: '', component: Start },
 { path: 'books', component: BookList },
-{ path: 'books/:isbn', component: BookDetail },
+{ path: 'books/:isbn', component: BookDetail, canActivate: [bookGuard] },
 { path: 'calculator', component: Calculator },
 { path: '**', redirectTo: '' },
 ];
