d6361cbaf959cb77fdf8fc8df96cd3243aa96ffe
Peter Dickten <peter.dickten@dcs-fuerth.de>
2025-11-13 13:12:33 +0100

    WIP

--- books/src/app/books/start/start.ts
+++ books/src/app/books/start/start.ts
@@ -1,11 +1,18 @@
+import { AsyncPipe } from '@angular/common';
 import { Component } from '@angular/core';
+import { Observable, timer } from 'rxjs';
 
 @Component({
   selector: 'book-start',
-  imports: [],
+  imports: [AsyncPipe],
   templateUrl: './start.html',
   styleUrl: './start.css',
 })
 export class Start {
 
+  obs3$: Observable<number> = timer(0,500);
+
+  constructor() {
+    this.obs3$.subscribe(console.log);
+  }
 }
