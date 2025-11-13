858dff30ab906d9352fd3700c534cc4704017315
Peter Dickten <peter.dickten@dcs-fuerth.de>
2025-11-13 11:46:32 +0100

    WIP

--- books/src/app/app.config.ts
+++ books/src/app/app.config.ts
@@ -1,12 +1,12 @@
 import { ApplicationConfig, provideBrowserGlobalErrorListeners, provideZoneChangeDetection } from '@angular/core';
-import { provideRouter, withHashLocation } from '@angular/router';
+import { provideRouter, withDebugTracing, withHashLocation } from '@angular/router';
 import { routes } from './app.routes';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideBrowserGlobalErrorListeners(),
     provideZoneChangeDetection({ eventCoalescing: true }),
-    provideRouter(routes) // , withDebugTracing() << debugging aktivieren
+    provideRouter(routes, withDebugTracing()) // , withDebugTracing() << debugging aktivieren
     // , withHashLocation() // hash-based routing
   ]
 };
